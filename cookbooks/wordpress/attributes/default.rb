82525550e86318f2ae23a2b5d737976e987baff0
Anton Butovich <abutovich@gmail.com>
2013-05-14 18:31:43 +0300

    changed WP install using wp-cli

--- cookbooks/wordpress/attributes/default.rb
+++ cookbooks/wordpress/attributes/default.rb
@@ -1,5 +1,10 @@
+default["wordpress"]["db_host"] = "localhost"
 default["wordpress"]["database"] = "wordpressdb"
 default["wordpress"]["db_username"] = "wpuser"
 default["wordpress"]["path"] = "/var/www/wordpress"
-default['wordpress']['server_name'] = "wordpress"
+default["wordpress"]["server_name"] = "localhost"
+default["wordpress"]["wp_title"] = "wordpress"
+default["wordpress"]["wp_admin"] = "admin"
+default["wordpress"]["wp_admin_email"] = nil
+default["wordpress"]["wp_admin_password"] = "password"
 default["wordpress"]["theme"] = nil
--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -14,7 +14,7 @@ include_recipe "php::module_mysql"
 include_recipe "apache2::mod_php5"
 include_recipe "mysql::ruby"
 
-["unzip", "wget"].each do |pkg|
+["unzip", "wget", "curl" ].each do |pkg|
   package pkg do
     action :install
   end
@@ -37,13 +37,13 @@ mysql_database_user node['wordpress']['db_username'] do
   action :grant
 end
 
-wordpress_latest = Chef::Config[:file_cache_path] + "/wordpress-latest.tar.gz"
-remote_file wordpress_latest do
-  source "http://wordpress.org/latest.tar.gz"
-  mode "0644"
+execute "wp-cli_install" do
+  command "curl http://wp-cli.org/installer.sh | bash"
+  creates "~/.composer/bin/wp"
+  action :run
 end
 
-directory node["wordpress"]["path"] do
+directory node['wordpress']['path'] do
   owner "root"
   group "root"
   mode "0755"
@@ -51,56 +51,78 @@ directory node["wordpress"]["path"] do
   recursive true
 end
 
-execute "untar-wordpress" do
+execute "wp-cli_download_wordpress" do
   cwd node['wordpress']['path']
-  command "tar --strip-components 1 -xzf " + wordpress_latest
-  creates node['wordpress']['path'] + "/wp-settings.php"
+  command "~/.composer/bin/wp core download"
+  creates node['wordpress']['path'] + "wp-settings.php"
 end
 
-if ( !node["wordpress"]["theme"].nil? )
-  wp_theme = Chef::Config[:file_cache_path] + "/theme.zip"
-  execute "unzip-theme" do
-    cwd ::File.join(node['wordpress']['path'], "wp-content", "themes")
-    command "unzip " + wp_theme
-    action :nothing
-  end
-  remote_file wp_theme do
-    source node["wordpress"]["theme"]
-    mode "0644"
-    notifies :run, "execute[unzip-theme]", :immediately
-  end
-end
+#wordpress_latest = Chef::Config[:file_cache_path] + "/wordpress-latest.tar.gz"
+#remote_file wordpress_latest do
+#  source "http://wordpress.org/latest.tar.gz"
+#  mode "0644"
+#end
 
-wp_secrets = Chef::Config[:file_cache_path] + '/wp-secrets.php'
 
-remote_file wp_secrets do
-  source 'https://api.wordpress.org/secret-key/1.1/salt/'
-  action :create_if_missing
-  mode 0644
-end
+#execute "untar-wordpress" do
+#  cwd node['wordpress']['path']
+#  command "tar --strip-components 1 -xzf " + wordpress_latest
+#  creates node['wordpress']['path'] + "/wp-settings.php"
+#end
 
-salt_data = ''
-ruby_block 'fetch-salt-data' do
-  block do
-    salt_data = File.read(wp_secrets)
-  end
-  action :create
-end
+#if ( !node["wordpress"]["theme"].nil? )
+#  wp_theme = Chef::Config[:file_cache_path] + "/theme.zip"
+#  execute "unzip-theme" do
+#    cwd ::File.join(node['wordpress']['path'], "wp-content", "themes")
+#    command "unzip " + wp_theme
+#    action :nothing
+#  end
+#  remote_file wp_theme do
+#    source node["wordpress"]["theme"]
+#    mode "0644"
+#    notifies :run, "execute[unzip-theme]", :immediately
+#  end
+  
+#  wp_theme_name = ::File.basename(node["wordpress"]["theme"]).split(".")[0]
+#  mysql_database "enable theme" do
+#    connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+#    database_name node["wordpress"]["theme"]
+#    sql <<EEND
+#       UPDATE wp_options SET option_value = '#{wp_theme_name}' WHERE option_name IN ('template', 'stylesheet', 'current_theme');
+#EEND
+#    action :query
+#end
 
-template node['wordpress']['path'] + '/wp-config.php' do
-  source 'wp-config.php.erb'
-  mode 0755
-  owner 'root'
-  group 'root' 
-  variables(
-    :database        => node['wordpress']['database'],
-    :user            => node['wordpress']['db_username'],
-    :password        => node['wordpress']['db_password'],
-    :wp_secrets      => salt_data)
-end
+#wp_secrets = Chef::Config[:file_cache_path] + '/wp-secrets.php'
 
-web_app 'wordpress' do
-  template 'site.conf.erb'
-  docroot node['wordpress']['path']
-  server_name node['wordpress']['server_name']
-end
+#remote_file wp_secrets do
+#  source 'https://api.wordpress.org/secret-key/1.1/salt/'
+#  action :create_if_missing
+#  mode 0644
+#end
+
+#salt_data = ''
+#ruby_block 'fetch-salt-data' do
+#  block do
+#    salt_data = File.read(wp_secrets)
+#  end
+#  action :create
+#end
+
+#template node['wordpress']['path'] + '/wp-config.php' do
+#  source 'wp-config.php.erb'
+#  mode 0755
+#  owner 'root'
+#  group 'root' 
+#  variables(
+#    :database        => node['wordpress']['database'],
+#    :user            => node['wordpress']['db_username'],
+#    :password        => node['wordpress']['db_password'],
+#    :wp_secrets      => salt_data)
+#end
+
+#web_app 'wordpress' do
+#  template 'site.conf.erb'
+#  docroot node['wordpress']['path']
+#  server_name node['wordpress']['server_name']
+#end
