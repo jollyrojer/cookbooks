d0b6f63fda0369af99b6cf19351bcd2be4aae80f
Anton Butovich <abutovich@gmail.com>
2013-05-15 14:11:15 +0300

    added apache vhost creation

--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -83,72 +83,8 @@ if ( !node["wordpress"]["theme"].nil? )
   end
 end
 
-#wordpress_latest = Chef::Config[:file_cache_path] + "/wordpress-latest.tar.gz"
-#remote_file wordpress_latest do
-#  source "http://wordpress.org/latest.tar.gz"
-#  mode "0644"
-#end
-
-
-#execute "untar-wordpress" do
-#  cwd node['wordpress']['path']
-#  command "tar --strip-components 1 -xzf " + wordpress_latest
-#  creates node['wordpress']['path'] + "/wp-settings.php"
-#end
-
-#if ( !node["wordpress"]["theme"].nil? )
-#  wp_theme = Chef::Config[:file_cache_path] + "/theme.zip"
-#  execute "unzip-theme" do
-#    cwd ::File.join(node['wordpress']['path'], "wp-content", "themes")
-#    command "unzip " + wp_theme
-#    action :nothing
-#  end
-#  remote_file wp_theme do
-#    source node["wordpress"]["theme"]
-#    mode "0644"
-#    notifies :run, "execute[unzip-theme]", :immediately
-#  end
-  
-#  wp_theme_name = ::File.basename(node["wordpress"]["theme"]).split(".")[0]
-#  mysql_database "enable theme" do
-#    connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
-#    database_name node["wordpress"]["theme"]
-#    sql <<EEND
-#       UPDATE wp_options SET option_value = '#{wp_theme_name}' WHERE option_name IN ('template', 'stylesheet', 'current_theme');
-#EEND
-#    action :query
-#end
-
-#wp_secrets = Chef::Config[:file_cache_path] + '/wp-secrets.php'
-
-#remote_file wp_secrets do
-#  source 'https://api.wordpress.org/secret-key/1.1/salt/'
-#  action :create_if_missing
-#  mode 0644
-#end
-
-#salt_data = ''
-#ruby_block 'fetch-salt-data' do
-#  block do
-#    salt_data = File.read(wp_secrets)
-#  end
-#  action :create
-#end
-
-#template node['wordpress']['path'] + '/wp-config.php' do
-#  source 'wp-config.php.erb'
-#  mode 0755
-#  owner 'root'
-#  group 'root' 
-#  variables(
-#    :database        => node['wordpress']['database'],
-#    :user            => node['wordpress']['db_username'],
-#    :password        => node['wordpress']['db_password'],
-#    :wp_secrets      => salt_data)
-#end
-
-#web_app 'wordpress' do
-#  template 'site.conf.erb'
-#  docroot node['wordpress']['path']
-#  server_name node['wordpress']['server_name']
-#end
+web_app 'phpapp' do
+  template 'site.conf.erb'
+  docroot node['phpapp']['path']
+  server_name node['phpapp']['server_name']
+end
