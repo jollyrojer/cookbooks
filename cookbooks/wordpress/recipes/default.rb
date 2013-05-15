e6988d34ffdb8d77ace4318c9805410a68fe738d
Anton Butovich <abutovich@gmail.com>
2013-05-15 13:45:15 +0300

    fixed --dbpath in wp_config block

--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -60,7 +60,7 @@ end
 
 execute "wp-cli_configure_db" do
   cwd node['wordpress']['path']
-  command "~/.composer/bin/wp core config --dbname=#{node["wordpress"]["database"]} --dbuser=#{node["wordpress"]["db_username"]} dbpass=#{node['wordpress']['db_password']} --dbhost=#{node["wordpress"]["db_host"]}"
+  command "~/.composer/bin/wp core config --dbname=#{node["wordpress"]["database"]} --dbuser=#{node["wordpress"]["db_username"]} --dbpass=#{node['wordpress']['db_password']} --dbhost=#{node["wordpress"]["db_host"]}"
   creates ::File.join(node['wordpress']['path'], "wp-config.php")
   action :run
 end
