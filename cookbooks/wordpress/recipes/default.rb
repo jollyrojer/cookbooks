980c98a0175a08b4ca2384f4957ccc407aa71742
Anton Butovich <abutovich@gmail.com>
2013-05-20 17:29:11 +0300

    Changing recipe to multinode (db, web)

--- cookbooks/wordpress/recipes/default.rb
+++ cookbooks/wordpress/recipes/default.rb
@@ -7,8 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "apache2"
-include_recipe "mysql::client"
-include_recipe "mysql::server"
+#include_recipe "mysql::client"
+#include_recipe "mysql::server"
 include_recipe "php"
 include_recipe "php::module_mysql"
 include_recipe "apache2::mod_php5"
@@ -22,19 +22,6 @@ end
 
 apache_site "default" do
   enable true
-end
-
-mysql_database node['wordpress']['database'] do
-  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
-  action :create
-end
-
-mysql_database_user node['wordpress']['db_username'] do
-  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
-  password node['wordpress']['db_password']
-  database_name node['wordpress']['database']
-  privileges [:select,:update,:insert,:create,:delete]
-  action :grant
 end
 
 wp_cli_installer = Chef::Config[:file_cache_path] + "/installer.sh"
---
+++ cookbooks/wordpress/recipes/mysql.rb
@@ -0,0 +1,23 @@
+#
+## Cookbook Name:: wordpress
+## Recipe:: mysql
+##
+## Copyright 2013, Cometera
+##
+## All rights reserved - Do Not Redistribute
+##
+include_recipe "mysql::client"
+
+mysql_database node['wordpress']['database'] do
+  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  action :create
+end
+
+mysql_database_user node['wordpress']['db_username'] do
+  connection ({:host => 'localhost', :username => 'root', :password => node['mysql']['server_root_password']})
+  password node['wordpress']['db_password']
+  database_name node['wordpress']['database']
+  privileges [:select,:update,:insert,:create,:delete]
+  action :grant
+end
+
